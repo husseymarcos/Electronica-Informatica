fc807ac53f378699ffbc224d7004db5475d3e959
carlix-27 <caacuna@mail.austral.edu.ar>
2023-05-23 21:32:57 -0300

    hola

---
+++ test/test/CardTest.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.Test;
+
+
+public class CardTest {
+    @Test
+    public void testGetBalance() { 
+        
+    }
+
+    @Test
+    public void testAddAmount() { }
+
+    @Test
+    public void testPlayWithInsuffFunds() { }
+
+    @Test
+    public void testPlay() { }
+
+    @Test
+    public void testTransferAmount() { }
+
+    @Test
+    public void testVerifyPassword() { }
+
+    @Test
+    public void testGamble() { }
+}
