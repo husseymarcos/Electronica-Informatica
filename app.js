12abda63488e06bbb6ad4018c866de515750e18b
carlix-27 <caacuna@mail.austral.edu.ar>
2024-05-02 16:11:16 -0300

    nodejs-mqtt-mongodb

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
 
+
+require('dotenv').config();
+
 // Middleware para analizar solicitudes JSON
 app.use(bodyParser.json());
 
-// Conexión a MongoDB en la nube - TODO Debo ver esto bien!
-const mongoURI = 'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<database>'; // Esto hay que ver si tiene sentido!
+
+
+
+// Conexión a MongoDB en la nube
+const mongoURI = process.env.MONGO_URI; // Atenco acá cambia la ip. 
 connectToMongoDB(mongoURI);
 
 // Conexión a MQTT
-const mqttURL = 'mqtt://52.23.254.208'; // O la URL de tu servidor MQTT en la nube. Atento acá a la ip que cambia constantemente.
+const mqttURL = process.env.MQTT_URL; // O la URL de tu servidor MQTT en la nube. Atento acá a la ip que cambia constantemente.
 const mqttClient = connectToMQTT(mqttURL);
 
 // Rutas de la aplicación
-app.use(bookRoutes);
+app.use("/books",bookRoutes);
 
-// Manejo de solicitudes POST
-app.post('/books', (req, res) => {
-  // Aquí puedes acceder a las funciones definidas en mongodb.js y mqttLogic.js
-  // Por ejemplo, puedes guardar los datos recibidos en MongoDB
-  // y publicar un mensaje MQTT si es necesario
-  res.send('Solicitud POST recibida');
-});
+
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
--- mongodb.js
+++ mongodb.js
@@ -10,30 +10,7 @@ async function connectToMongoDB(mongoURI) {
     }
 }
 
-// Define tus modelos y esquemas de Mongoose aquí, por ejemplo:
-const bookSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    pages: Number,
-});
-
-const Book = mongoose.model('Book', bookSchema);
-
-// Funciones para realizar operaciones CRUD en la base de datos
-async function createBook(bookData) {
-    try {
-        const newBook = new Book(bookData);
-        await newBook.save();
-        console.log('Libro creado exitosamente:', newBook);
-        return newBook;
-    } catch (error) {
-        console.error('Error al crear el libro:', error);
-    }
-}
-
-// Exporta las funciones que quieras utilizar en otros archivos
+// Exporta la función para conectar a MongoDB
 module.exports = {
-    connectToMongoDB,
-    createBook,
-    // Agrega otras funciones CRUD aquí según sea necesario
+    connectToMongoDB
 };
