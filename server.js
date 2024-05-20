bc0d4ef00d038f96cd570efcb2faba2ba104b40e
carlix-27 <caacuna@mail.austral.edu.ar>
2024-05-20 19:32:20 -0300

    Delete files
    Realizo la misma lógica que hizo fernando en relación al mongodb, voy a ver si funciona como tal!

--- server.js
+++ server.js
@@ -1,108 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-require('dotenv').config();
-const config = require('./config');
-const app = express();
+// import { MongoClient } from "mongodb";
+const { MongoClient } = require('mongodb')
+// Replace the uri string with your MongoDB deployment's connection string.
+var config   = require('./config');
+var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
+var date_time = new Date();
+// Create a new client and connect to MongoDB
+const client = new MongoClient(mongoUri);
 
-app.use(bodyParser.json());
-
-// Definimos la estructura del libro usando Mongoose
-const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: String,
-  year: Number
-});
-
-const Book = mongoose.model('Book', bookSchema);
-
-// Middleware para CORS y Content Security Policy
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Content-Security-Policy', "default-src 'none'; img-src 'self' http://54.87.45.60:1883");
-    next();
-});
-
-app.use(express.static(path.join(__dirname, 'static')));
-
-// Conexión a MongoDB usando Mongoose
-const mongoURI = `mongodb://${config.mongodb.hostname}:${config.mongodb.port}/${config.mongodb.database}`;
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Conexión a MongoDB establecida correctamente'))
-  .catch(error => console.error('Error al conectaor a MongoDB:', error));
-
-// Lógica de MQTT
-const mqttURL = `mqtt://${config.mqtt.hostname}:${config.mqtt.port}`;
-const mqtt = require('mqtt');
-
-function connectToMQTT(mqttURL) {
-    const mqttClient = mqtt.connect(mqttURL);
-
-    mqttClient.on('connect', () => {
-        console.log('Conectado al servidor MQTT');
-        mqttClient.subscribe('/api/books', (err) => {
-            if (err) {
-                console.error('Error al suscribirse:', err);
-            } else {
-                console.log('Suscripción exitosa a /api/books');
-            }
-        });
-    });
-
-    mqttClient.on('message', async (topic, message) => {
-        console.log('Mensaje recibido:', message.toString(), 'en el topic', topic);
-        
-        if (topic === 'api/books') {
-            try {
-                const bookData = JSON.parse(message.toString());
-                const book = new Book(bookData);
-                const result = await book.save();
-                console.log(`Libro guardado en la base de datos con el _id: ${result._id}`);
-            } catch (error) {
-                console.error('Error al guardar el libro', error);
-            }
-        }
-    });
-
-    return mqttClient;
-}
-
-connectToMQTT(mqttURL);
-
-
-// Ruta para agregar libros
-app.post('/api/books/publish', async (req, res) => {
-    try {
-        const { title, author, genre, year } = req.body;
-        const bookData = {
-            title: title,
-            author: author,
-            genre: genre,
-            year: year
-        }
-
-        const book = new Book(bookData);
-        const result = await book.save();
-        res.status(201).json({ message: `Libro guardado con el _id: ${result._id}` });
-
-    } catch (error) {
-        res.status(500).json({ message: `Error al guardar en la base de datos: ${error.message}` });
+async function run() {
+  try {
+    // Connect to the "insertDB" database and access its "haiku" collection
+    const database = client.db(config.mongodb.database);
+    const haiku = database.collection("message");
+    
+    // Create a document to insert
+    const doc = {
+//      title: "Record of a Shriveled Datum",
+      fecha: date_time,
+      content: "No bytes, no problem. Just insert a document, in MongoDB",
     }
-});
+    // Insert the defined document into the "haiku" collection
+    const result = await haiku.insertOne(doc);
 
-// Iniciar el servidor
-const PORT = process.env.PORT || 27017;
-app.listen(PORT, () => {
-    console.log(`Servidor Express.js en funcionamiento en el puerto ${PORT}`);
-});
-
-
-
-
-
-
+    // Print the ID of the inserted document
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+     // Close the MongoDB client connection
+    await client.close();
+  }
+}
+// Run the function and handle any errors
+run().catch(console.dir);
