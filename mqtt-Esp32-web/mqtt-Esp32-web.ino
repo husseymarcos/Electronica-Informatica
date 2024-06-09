462934e123ad0b1b5c749a66361131ef559490a0
carlix-27 <caacuna@mail.austral.edu.ar>
2024-06-09 12:25:40 -0300

    Update mqtt-Esp32-web.ino

--- mqtt-Esp32-web/mqtt-Esp32-web.ino
+++ mqtt-Esp32-web/mqtt-Esp32-web.ino
@@ -122,7 +122,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 // Reconecta con MQTT broker
 void reconnect() {
   // MQTT_CLIENT.setServer("192.168.1.206", 1883); // si uso un servidor local <ver IP correcta>
-  MQTT_CLIENT.setServer("54.173.227.140", 1883);  // servidor gratuito
+  MQTT_CLIENT.setServer("54.234.95.123", 1883);  // servidor gratuito
 
   MQTT_CLIENT.setClient(WIFI_CLIENT);
 
