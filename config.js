bd85c95e320a17d145f039c9b34c8829cb1c2f4e
carlix-27 <caacuna@mail.austral.edu.ar>
2024-06-09 12:22:55 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.debug = process.env.DEBUG || true;
 
 config.mqtt  = {};
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || '#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '54.173.227.140'; // Public IP
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '54.234.95.123'; // Public IP
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
 
 config.mongodb = {};
