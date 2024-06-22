964fd2747becff495e07261614a39330235d5c33
carlix-27 <caacuna@mail.austral.edu.ar>
2024-06-22 12:42:12 -0300

    Update config.js
    Change public ip mqtt

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.debug = process.env.DEBUG || true;
 
 config.mqtt  = {};
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || '#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '3.92.240.228'; // public IP
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '54.156.53.99'; // public IP
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
 
 config.mongodb = {};
